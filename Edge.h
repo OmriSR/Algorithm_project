9278b08260a99201dfc067bb44be35e1e237e0bd
rogerklein9 <g.aviezri@gmail.com>
2022-04-05 20:18:17 +0300

    -Vertex no longer templated... redundant -Edge and Vertex Nested in Graph (Edge is public hence Graph::Edge) -Quicksort implemented (untested) -some lines commented out inside minheap for the proj build.

--- Edge.h
+++ Edge.h
@@ -1,30 +1,8 @@
 #pragma once
 #include <utility>
 #include <vector>
+#include "Graph.h"
 
-class Edge
-{
-	//std::pair<int, int> m_name;
-	unsigned int src;
-	unsigned int dst;
-	int m_weight = 0;
-	bool m_ignore = false;
-	bool m_isBridge;
 
-public:
-
-	int getWeight()
-	{
-		return m_weight;
-	}
-
-	void setWeight(const int& i_weight)
-	{
-		m_weight = i_weight;
-	}
-
-	bool operator<(const Edge& Other) { return m_weight < Other.m_weight; }//for sorting edges with kruskal
-};
-
-void quicksort(std::vector<Edge>& lst, std::vector<Edge>::iterator Left, std::vector<Edge>::iterator Right);
-std::vector<Edge>::iterator partition(std::vector<Edge>& lst, std::vector<Edge>::iterator Left, std::vector<Edge>::iterator Right);
+void quicksort(std::vector<Graph::Edge>& lst, std::vector<Graph::Edge>::iterator Left, std::vector<Graph::Edge>::iterator Right);
+std::vector<Graph::Edge>::iterator partition(std::vector<Graph::Edge>& lst, std::vector<Graph::Edge>::iterator Left, std::vector<Graph::Edge>::iterator Right);
--- MinHeap.h
+++ MinHeap.h
@@ -12,7 +12,7 @@ class MinHeap
 	{
 		int m_vertexNum = 0;						//edge name  == data
 		int m_weight = 0;						   //edge weight == key
-		Edge* m_dualPointer = nullptr;
+//		Edge* m_dualPointer = nullptr;
 
 	public:
 		HeapNode(int i_vertexNum = 0, int i_weight = 0);
@@ -20,67 +20,30 @@ class MinHeap
 		void setVertexNum(int i_vertex_num);
 		int getWeight();
 		void setWeight(int i_newWeight);
-		void setDoualPtr(Edge* twinInEdgeArr);
+//		void setDoualPtr(Edge* twinInEdgeArr);
 		friend MinHeap;
 	};
 
 	HeapNode* m_heapArr = nullptr;
 	int m_heapSize = 0, m_heapMaxSize = 0; /* = n-1*/  // the heap is constructed of all the edges from some vertex v in the graph to the current tree -> <= n-1 in a simple graph
-	int* m_heapPtrArr = nullptr;  
+	int* m_heapPtrArr = nullptr;
 	bool m_allocated = false;
 
 	int getLeftSon(int i_heapNode);
 
 	int getRightSon(int i_heapNode);
 
-		if (min != i_heapNode)
-		{
+	int getParent(int i_heapNode);
 
 	void fixHeap(int i_heapNode);
 
+	void updateHeapNodesDualPtr(const int& i_ind1, const int& i_ind2);
 
 	void initHeapPtrArr(HeapNode* i_heapBaseArr, int i_size);
 
-		for (int i = 0; i < i_size; ++i)
-		{
-			m_edgesWeightsAndHeapPtr[i].setWeight(i_heapBaseArr[i].getWeight());
-			i_heapBaseArr[i].setDoualPtr(m_edgesWeightsAndHeapPtr + i);
-		}
-	}
-
 public:
 
-	void build(HeapNode* i_heapBaseArr, int i_size)
-	{
-		MinHeap(i_heapBaseArr, i_size);
-	}
-
-	MinHeap(int i_numOfVertices)
-	{
-		m_heapMaxSize = i_numOfVertices - 1;
-		m_heapArr = new HeapNode[m_heapMaxSize];
-		m_edgesWeights = new Edge[m_heapMaxSize];
-		m_allocated = true;
-	}
-
-	MinHeap(HeapNode* i_heapBaseArr, int i_size)
-	{
-		m_heapMaxSize = m_heapSize = i_size;
-
-		m_heapArr = i_heapBaseArr;
-
-		for (int i = 0; i < i_size; ++i)
-		{
-			m_heapArr[i].getVertexNum(i);
-		}
-
-		initEdgesWeightsAndHeapPtrArr(m_heapArr, i_size);
-
-		for (int i = (i_size / 2 - 1); i >= 0; --i)
-		{
-			fixHeap(i);
-		}	
-	}
+	void build(HeapNode* i_heapBaseArr, int i_size);
 
 	MinHeap(int i_numOfVertices);
 
@@ -100,11 +63,4 @@ public:
 
 	void climbUp(int i_node);
 
-	void decreaseKey(int i_nodeIndex, int i_newWeight)
-	{
-		m_edgesWeightsAndHeapPtr[i_nodeIndex].setWeight(i_newWeight);
-		nodeInHeap->setWeight(i_newWeight);
-		fixHeap(nodeInHeap->getVertexNum());
-	}
 };
-
